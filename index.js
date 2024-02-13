89856aede8a22dff9132da4d8a440d0e0c708cdc
nafis200 <nafisahamed14@gmail.com>
2024-02-13 07:33:05 -0500

    function simple commit

--- index.js
+++ index.js
@@ -17,20 +17,12 @@ function Angle(){
 
 
 function calculateTriangle(){
-    const triangleBase = document.getElementById('triangle-base')
-    const triangleBaseValue = triangleBase.value;
-    const base = parseFloat(triangleBaseValue)
-    
-    const triangleHeight = document.getElementById('triangle-height')
-    const triangleHeightValue = triangleHeight.value
-    const Height = parseFloat(triangleHeightValue)
-   
+    const base = allcalculated('triangle-base')
+    const Height = allcalculated('triangle-height')
     const area = 0.5 * base * Height;
     console.log('area of triangle: ',area)
-   
     const triangleArea = document.getElementById('triangle-area');
     triangleArea.innerText = area
-
     const calculated = document.getElementById('calculated')
     calculated.innerText = area;
     calculated.value =' ';
