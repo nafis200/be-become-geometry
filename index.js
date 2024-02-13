51634cd857e6e6eb07d17c4f6f3ebc03ef6bc4b2
nafis200 <nafisahamed14@gmail.com>
2024-02-13 06:55:26 -0500

    third commit

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function calculateTriangle(){
     const triangleBase = document.getElementById('triangle-base')
     const triangleBaseValue = triangleBase.value;
-    console.log(triangleBaseValue)
+    const base = parseFloat(triangleBaseValue)
+    
+    const triangleHeight = document.getElementById('triangle-height')
+    const triangleHeightValue = triangleHeight.value
+    const Height = parseFloat(triangleHeightValue)
+   
+    const area = 0.5 * base * Height;
+    console.log('area of triangle: ',area)
+   
+    const triangleArea = document.getElementById('triangle-area');
+    triangleArea.innerText = area
+
 }
