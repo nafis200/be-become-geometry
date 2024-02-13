7ff2777881d2e04f1e7e72033d00e3c6e934b783
nafis200 <nafisahamed14@gmail.com>
2024-02-13 06:33:01 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function calculateTriangle(){
+    const triangleBase = document.getElementById('triangle-base')
+    const triangleBaseValue = triangleBase.value;
+    console.log(triangleBaseValue)
+}
